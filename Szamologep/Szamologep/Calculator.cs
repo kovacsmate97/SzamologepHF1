95da96283ca9d8843524d663dda793392305f3d1
kovacsmate97 <kovimatyi8@freemail.hu>
2017-02-13 18:16:10 +0100

    Hozzáadva alap műveletek

---
+++ Szamologep/Szamologep/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static double Osszead(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Kivon(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Szorzas(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Osztas(double x, double y)
+        {
+            return x / y;
+        }
+        public static void CE(double y)
+        {
+            y = 0;
+            return;
+        }
+        public static void C(double x, double y)
+        {
+            x = 0;
+            y = 0;
+            return;
+        }
+    }
+}
